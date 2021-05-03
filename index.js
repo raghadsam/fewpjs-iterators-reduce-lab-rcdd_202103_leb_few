d74f68b2c3130dd9df315cfa887e5dd61134ea7c
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-05-03 21:15:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-//let totalBatteries=0;
+let totalBatteries=0;
 const reducer=function(totalBatteries,batteryBatche){
-  let totalBatteries=0;
+  //let totalBatteries=0;
     return totalBatteries+batteryBatche;
 
 }
